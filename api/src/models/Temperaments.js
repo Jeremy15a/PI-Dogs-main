84a97ff45f00d874d1c4cd2ebafb5d036e67711e
JAmadorMarquina <jeremy.amador15@gmail.com>
2023-05-20 17:48:30 -0500

    modelos terminados

--- api/src/models/Temperaments.js
+++ api/src/models/Temperaments.js
@@ -2,29 +2,18 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  sequelize.define('dog', {
+  sequelize.define('temperaments', {
     id:{
       type: DataTypes.UUID,
       defaultValue: sequelize.UUIDV4,
       allowNull: false,
       primaryKey: true,
     },
-    reference_image_id:{
-      type: DataTypes.NUMBER,
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    height:{
-      type: DataTypes.NUMBER
-    },
-    weight:{
-      type: DataTypes.NUMBER
-    },
-    life_span:{
-      type: DataTypes.NUMBER
-    },
+
   },
   { timestamps: false }
   );
